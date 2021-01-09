ab0649eafdcbb24d5b2d14c0041d83f5e7aeef6a
im-ant <anthony.gx.chen@gmail.com>
2021-01-09 13:14:15 -0500

    updated the get features, rewards and transition matrices methods of this environment to exclude terminal states

--- linear/envs/random_walk_chain.py
+++ linear/envs/random_walk_chain.py
@@ -83,75 +83,51 @@ class RandomWalkChainEnv(gym.Env):
     def get_num_states(self):
         """
         Helper function to get the number of underlying states in this env
-        NOTE: +1 to include the termination state
+        NOTE: -1 to exclude terminal states
         :return: int of number of states
         """
-        return self.n_states + 1
+        return self.n_states - 1
 
     def get_transition_matrix(self):
         """
-        Helper function to return the transition matrix for this env
-        :return: (self.n_states+1, self.n_states+1) np matrix
+        Helper function to return the transition matrix for this env,
+        excluding terminal states
+        :return: (self.n_states-1, self.n_states-1) np matrix
         """
         # Transition matrix
-        p_n_states = self.n_states + 1
+        p_n_states = self.n_states - 1
         P_trans = np.zeros((p_n_states, p_n_states))
         for i in range(1, (p_n_states-1)):
             P_trans[i, i + 1] = 0.5
             P_trans[i, i - 1] = 0.5
-        P_trans[0, 0] = 0.0
-        P_trans[self.n_states, self.n_states] = 0.0
+        P_trans[0, 1] = 0.5
+        P_trans[p_n_states-1, p_n_states-2] = 0.5
 
         return P_trans
 
     def get_reward_function(self):
         """
         Helper function to get the (tabular) reward function for each state
-        NOTE: charactering each state's reward by the expected reward
-              from transitioning out of the state. This may be slightly
-              different from the TD way of solving for state values.
-        :return: (self.n_states+1,) vector
+        NOTE: each state's reward is set to the _expected_ reward received
+              from transitioning _out_ of the state.
+        :return: (self.n_states-1,) vector
         """
-        p_n_states = self.n_states + 1
+        p_n_states = self.n_states - 1
         R_fn = np.zeros(p_n_states)
-        R_fn[-1] = 1.0
+        # .5 chance of transitioning left (reward = 0), .5 chance of
+        # terminating via transitioning right (reward = 1)
+        R_fn[-1] = 0.5
 
         return R_fn
 
     def get_feature_matrix(self):
         """
         Helper function to get the state to features mapping matrix
-        :return: (self.n_states+1, self.feature_dim) np matrix
+        :return: (self.n_states-1, self.feature_dim) np matrix
         """
-        # Get feature matrix (p_n_states, feature_dim)
-        p_n_states = self.n_states + 1
-        phi_mat = np.empty((p_n_states, self.feature_dim))
-        for s_n in range(p_n_states):
-            phi_mat[s_n] = self.state_2_features(s_n)
-        return phi_mat
-
-    def solve_linear_reward_parameters(self):
-        """
-        Helper function to solve for the best-fit linear parameters for the
-        reward function.
-        :return: (self.feature_dim, ) parameters for reward fn
-        """
-        p_n_states = self.n_states + 1
-
-        # Get feature matrix (p_n_states, feature_dim)
-        phi_mat = np.empty((p_n_states, self.feature_dim))
-        for s_n in range(p_n_states):
-            phi_mat[s_n] = self.state_2_features(s_n)
-
-        # Get reward function
-        r_vec = self.get_reward_function()
-
-        # Solve parameters from Moore–Penrose inverse
-        phi_mat_T = np.transpose(phi_mat)
-        mp_inv = np.linalg.inv((phi_mat_T @ phi_mat)) @ phi_mat_T
-        bf_Wr = mp_inv @ r_vec
-
-        return bf_Wr
+        # Tabular features
+        p_n_states = self.n_states - 1
+        return np.identity(p_n_states)
 
     def render(self):
         pass
